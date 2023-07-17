9d9b2eda28dfa2fa5c784e7ba693ac349385d981
Pedro Monteiro <pedrormonteiro@hotmail.com>
2023-07-17 10:53:58 +0100

    basic unit test

---
+++ src/components/MenuButton.test.tsx
@@ -0,0 +1,24 @@
+import { fireEvent, render } from "@testing-library/react";
+import MenuButton from "./MenuButton";
+
+describe("Menu Button", () => {
+  test("renders without error", () => {
+    render(<MenuButton />);
+  });
+
+  test("clicking the button calls showAlert function", () => {
+    const { getByRole } = render(<MenuButton />);
+    const button = getByRole("button");
+
+    const mockAlert = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+    fireEvent.click(button);
+
+    expect(mockAlert).toHaveBeenCalledTimes(1);
+    expect(mockAlert).toHaveBeenCalledWith(
+      "this component was created just to demonstrate the usage of a client component within a next.js 13 environment"
+    );
+
+    mockAlert.mockRestore();
+  });
+});
